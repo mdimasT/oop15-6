96fac9f4cae09cf4bc6ac7d64671611d02da5393
MaxDimas <maxdimas@outlook.com>
2023-06-15 17:44:59 -0600

    feat: class warrior

--- game2.py
+++ game2.py
@@ -20,3 +20,20 @@ class Character:
     
     def isKnockedOut(self):
         return self.newHP <= 0
+
+# Define specific character classes with different types and attacks
+class Warrior(Character):
+    def __init__(self, name):
+        super().__init__(name, "Warrior", 100)
+    
+    def attack(self, target):
+        attackDamage = random.randint(10, 20)
+        target.takeDamage(attackDamage)
+        print(f"{self.name} attacked {target.name} with a sword for {attackDamage} damage!")
+    
+    def defend(self):
+        defenseBonus = random.randint(5, 10)
+        self.newHP += defenseBonus
+        if self.newHP > self.maxHP:
+            self.newHP = self.maxHP
+        print(f"{self.name} defended and restored {defenseBonus} health.")
